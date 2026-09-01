e965ad417a54d2900d59cfc5946a98584478fad7
Aryan Sharma <aryansharma772004@gmail.com>
2026-09-01 21:07:55 +1000

    Adding Signup screen

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -7,7 +7,7 @@ import { StackTitle } from 'expo-router/build/layouts/stack-utils';
 
 import { useEffect, useState } from 'react';
 import { useRouter, useSegments } from 'expo-router';
-import { supabase } from '@/lib/supabase';
+import { supabase } from '@/lib/supabse';
 import type { Session } from '@supabase/supabase-js';
 
 SplashScreen.preventAutoHideAsync();
@@ -17,6 +17,43 @@ export default function TabLayout() {
 
   const [session, setSession] = useState<Session | null>(null);
   const [loading, setLoading] = useState(true);
+
+  const router = useRouter();
+  const segments = useSegments();
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+      setLoading(false);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+
+    const inAuthScreen = segments[0] === 'login' || segments[0] === 'signup';
+
+    if (!session && !inAuthScreen) {
+      router.replace('/login');
+    }
+
+    if (session && inAuthScreen) {
+      router.replace('/');
+    }
+  }, [session, loading, segments]);
+
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <AnimatedSplashOverlay />
